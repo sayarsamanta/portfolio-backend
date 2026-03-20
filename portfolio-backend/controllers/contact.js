e3b39415d7399de5258ed6102a15ef346f3db00a
Sayar Samanta <sayarsamanta@gmail.com>
2026-03-20 14:58:49 +0530

    1. Added sender email in env

--- portfolio-backend/controllers/contact.js
+++ portfolio-backend/controllers/contact.js
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async ({ name, email, message }) => {
   const data = await resend.emails.send({
-    from: "Portfolio Contact <contact@sayarsamanta.dev>",
+    from: process.env.SENDER_EMAIL,
     to: "sayarsamanta@gmail.com",
     reply_to: email,
     subject: `Portfolio Contact from ${name}`,
